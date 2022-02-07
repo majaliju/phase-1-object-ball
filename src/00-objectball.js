a254787c9c03f19b338991ce0b7e89a1238e49a4
majaliju <majalijunyc@gmail.com>
2022-02-07 17:19:41 -0500

    current update

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -19,7 +19,7 @@ function gameObject(){
                     shoe: 14,
                     points: 12,
                     rebounds: 12,
-                    assits: 12,
+                    assists: 12,
                     steals: 12,
                     blocks: 12,
                     slamDunks: 7
@@ -29,7 +29,7 @@ function gameObject(){
                     shoe: 17,
                     points: 17,
                     rebounds: 19,
-                    assits: 10,
+                    assists: 10,
                     steals: 3,
                     blocks: 1,
                     slamDunks: 15
@@ -39,7 +39,7 @@ function gameObject(){
                     shoe: 19,
                     points: 26,
                     rebounds: 12,
-                    assits: 6,
+                    assists: 6,
                     steals: 3,
                     blocks: 8,
                     slamDunks: 5
@@ -49,7 +49,7 @@ function gameObject(){
                     shoe: 15,
                     points: 19,
                     rebounds: 2,
-                    assits: 2,
+                    assists: 2,
                     steals: 4,
                     blocks: 11,
                     slamDunks: 1
@@ -75,7 +75,7 @@ function gameObject(){
                     shoe: 16,
                     points: 12,
                     rebounds: 4,
-                    assits: 7,
+                    assists: 7,
                     steals: 7,
                     blocks: 15,
                     slamDunks: 10
@@ -85,7 +85,7 @@ function gameObject(){
                     shoe: 14,
                     points: 24,
                     rebounds: 12,
-                    assits: 12,
+                    assists: 12,
                     steals: 4,
                     blocks: 5,
                     slamDunks: 5
@@ -95,7 +95,7 @@ function gameObject(){
                     shoe: 15,
                     points: 33,
                     rebounds: 3,
-                    assits: 2,
+                    assists: 2,
                     steals: 1,
                     blocks: 1,
                     slamDunks: 0
@@ -105,7 +105,7 @@ function gameObject(){
                     shoe: 15,
                     points: 6,
                     rebounds: 12,
-                    assits: 12,
+                    assists: 12,
                     steals: 22,
                     blocks: 5,
                     slamDunks: 12
@@ -116,7 +116,37 @@ function gameObject(){
     
 }
 
+// create a function that holds a new object of purely players
+function players(){
+    // optimal Flatiron walk-thru v1
+    const game = gameObject();
 
+    //Object. assign --> make an object that holds all the players
+    const homePlayers = game.home.players
+    const awayPlayers = game.away.players
+    return Object.assign(homePlayers, awayPlayers)
+    
+}
+
+// flatIron walk-thru method
+function numPointsScoredv2(playerInput){
+   
+
+    for (const playerName in players()){ // iterate thru players
+        if (playerName === playerInput){
+            return players()[playerName].points
+        }
+    }
+}
+
+
+
+
+
+
+
+
+// Majlind's natural method for numPointsScored
 function numPointsScored(player){
     const game = gameObject();  // access the gameObject 
     for (const gameKey in game){ // enter one level below
@@ -172,55 +202,44 @@ function teamNames(){
 
 function playerNumbers(teamName){
     const game = gameObject(); // access the gameObject
-    const jerseyArray = []; 
-    debugger;
+    const jerseyArray = [];
 
     for (const gameKey in game){ // enter the 'home' and 'array' object
-        debugger;
         const teamNameObj = game[gameKey]["teamName"]
         const playersObj = game[gameKey]["players"]
-        debugger;
         if (teamName === teamNameObj){ // if teamName matches the teamName in our game object
             for (const player in playersObj){
-                console.log([`${player}`])
-                // issue is here with undefined being pushed into jerseyArray
-                jerseyArray.push([`${player}`]["number"]) // [player][number] is undefined 
-                debugger;
+                console.log(playersObj[player]["number"])
+                jerseyArray.push(playersObj[player]["number"]) //
             }
             }
-            // jerseyArray.push(playersObj["number"]) // push that player's jersey number 
-            debugger;
         }
+        return jerseyArray
 }
 
-playerNumbers("Brooklyn Nets")
-playerNumbers("Charlotte Hornets")
+
+function playerStats(player){
+    const game = gameObject();  // access the gameObject 
+    for (const gameKey in game){ // enter one level below
+        const playersObj = game[gameKey]["players"] // enter another level below, to the players object
+        for (const thisPlayer in playersObj){ 
+            if (thisPlayer === player){ // checks if player-input matches the given player
+                return playersObj[player]
+            }
+            
+        }
+    }
+}
+
+function bigShoeRebounds(){
+    // want to figure a clean method to cross-check the shoe-size for this
+    console.log(Object.values(players()[]))
+    return Object.values(players())
+}
+bigShoeRebounds();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
